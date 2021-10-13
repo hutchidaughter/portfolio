cbb21ccc943c851d1dc973f474bf2e6c0649f217
hutchidaughter <88189530+hutchidaughter@users.noreply.github.com>
2021-10-13 21:09:03 +0300

    adding js

---
+++ js/contact.js
@@ -0,0 +1,9 @@
+let firstName = document.getElementById("firstName").value;
+let lastName = document.getElementById("lastName");
+let email = document.getElementById("email");
+let phoneNumber = document.getElementById("phoneNumber");
+let dateForOurMeeting = document.getElementById("dateForOurMeeting");
+let additionalComments = document.getElementById("additionalComments");
+let submitButton = document.getElementById("submitButton");
+
+console.log(firstName);
